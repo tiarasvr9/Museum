07854eff8e5ff110f67c123091a7e0496bc3f709
tiarasvr9 <tiarasav01@gmail.com>
2020-12-11 18:49:37 +0700

    first commit

---
+++ appConfig.js
@@ -0,0 +1,48 @@
+var developmentDatabase = {
+    postgres: {
+    host: 'localhost',
+    port: 5432,
+    database: 'database_name',
+    user: 'postgres',
+    password: 'postgres'
+    }
+    }
+    
+    var connectionString = "aqtikmqsukjakj:c782673a0b9ebe2f4006d3730f38bf5de6429cf76c962877c956b799c387259d@ec2-34-246-141-162.eu-west-1.compute.amazonaws.com:5432/d8bmkjiv0leil5?ssl=true";
+    if (process.env.NODE_ENV == 'production') {
+    //Production mode
+    if (process.env.DATABASE_URL) {
+    developmentDatabase =
+    parseConnectionString(process.env.DATABASE_URL);
+    } else {
+    console.log("process.env.DATABASE_URL empty, connectionStringvariable used");
+    developmentDatabase = parseConnectionString(connectionString);
+    }
+    }else{
+    //Development mode
+    developmentDatabase = parseConnectionString(connectionString);
+    }
+    function parseConnectionString(connectionString) {
+    if (connectionString) {
+    var myRegexp = /(\w+):(\w+)@(.+):(\w+)\/(\w+)/g;
+    var match = myRegexp.exec(connectionString);
+    if (match.length == 6) {
+    developmentDatabase.postgres.user = match[1];
+    developmentDatabase.postgres.password = match[2];
+    developmentDatabase.postgres.host = match[3];
+    developmentDatabase.postgres.port = Number(match[4]);
+    developmentDatabase.postgres.database = match[5];
+    developmentDatabase.postgres.ssl = developmentDatabase.postgres.ssl = { rejectUnauthorized: false };
+    return developmentDatabase;
+    }
+    }
+    console.log("connectionString cannot be parsed");
+    return null;
+    }
+    module.exports = {
+    hostname: "http://localhost",
+    port: 5656,
+    database: {
+    postgres: developmentDatabase.postgres
+    }
+    }
---
+++ database.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+var promise = require('bluebird');
+var CONFIG = require('./appConfig');
+var pgp = require('pg-promise')(options);
+var DATABASE_PGB = pgp(CONFIG.database.postgres);
+
+module.exports = {
+       getAllLocations: getAllLocations
+};
+
+var options = {
+    promiseLib: promise
+};
+
+function getAllLocations(cb) {
+      DATABASE_PGB.any('SELECT ST_X(lokasi) as longitude, ST_Y(lokasi) as latitude from museum')
+      .then(function (data) {
+         cb(null, data);})
+       .catch(function (err) {
+          cb(err)});
+}
